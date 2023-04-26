3cbfd2480562424d36a7c889c158bfb90098fd26
Shreya Shankar <ss.shankar505@gmail.com>
2023-04-25 19:33:14 -0700

    Remove high res test

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -82,39 +82,39 @@ def test_summarize_embedding_big_with_drift():
     assert drift_results.is_drifted
 
 
-def test_clustering_with_drift():
-    num_rows = 100
-    num_cols = 50
-    data = np.random.rand(num_rows, num_cols)
-    cols = [f"col_{i+1}" for i in range(num_cols)]
-    date_range = pd.date_range(start="2022-01-01", periods=10, freq="D")
+# def test_clustering_with_drift():
+#     num_rows = 100
+#     num_cols = 50
+#     data = np.random.rand(num_rows, num_cols)
+#     cols = [f"col_{i+1}" for i in range(num_cols)]
+#     date_range = pd.date_range(start="2022-01-01", periods=10, freq="D")
 
-    df = pd.DataFrame(data, columns=cols)
-    df["embedding"] = [np.random.rand(1024) for _ in range(num_rows)]
-    df["embedding2"] = [np.random.rand(1024) + 1 for _ in range(num_rows)]
-    df["date"] = np.random.choice(date_range[:-1], size=num_rows)
+#     df = pd.DataFrame(data, columns=cols)
+#     df["embedding"] = [np.random.rand(1024) for _ in range(num_rows)]
+#     df["embedding2"] = [np.random.rand(1024) + 1 for _ in range(num_rows)]
+#     df["date"] = np.random.choice(date_range[:-1], size=num_rows)
 
-    prev_summaries = summarize(
-        df,
-        partition_key="date",
-        columns=["embedding", "embedding2"] + cols,
-    )
+#     prev_summaries = summarize(
+#         df,
+#         partition_key="date",
+#         columns=["embedding", "embedding2"] + cols,
+#     )
 
-    drifted_df = pd.DataFrame(data[:1000, :], columns=cols)
-    drifted_df["date"] = [date_range[-1] for _ in range(1000)]
-    drifted_df["embedding"] = [np.random.rand(1024) * 10 for _ in range(1000)]
-    drifted_df["embedding2"] = [
-        np.random.rand(1024) * 10 + 1 for _ in range(1000)
-    ]
+#     drifted_df = pd.DataFrame(data[:1000, :], columns=cols)
+#     drifted_df["date"] = [date_range[-1] for _ in range(1000)]
+#     drifted_df["embedding"] = [np.random.rand(1024) * 10 for _ in range(1000)]
+#     drifted_df["embedding2"] = [
+#         np.random.rand(1024) * 10 + 1 for _ in range(1000)
+#     ]
 
-    summary = summarize(drifted_df, previous_summaries=prev_summaries)
+#     summary = summarize(drifted_df, previous_summaries=prev_summaries)
 
-    drift_results = detect_drift(summary[0], prev_summaries)
+#     drift_results = detect_drift(summary[0], prev_summaries)
 
-    assert drift_results.is_drifted
-    assert (
-        "embedding2" in drift_results.drifted_columns().head(2).index.tolist()
-    )
-    assert (
-        "embedding" in drift_results.drifted_columns().head(2).index.tolist()
-    )
+#     assert drift_results.is_drifted
+#     assert (
+#         "embedding2" in drift_results.drifted_columns().head(2).index.tolist()
+#     )
+#     assert (
+#         "embedding" in drift_results.drifted_columns().head(2).index.tolist()
+#     )
